77f7d056024c9718ff827650df621a73370cf6cd
b150005 <b150005@outlook.jp>
2024-11-22 13:20:01 +0900

    ⚡️ 画像をシリアライズする際のパフォーマンスを改善

--- lib/src/utils/image_utils.dart
+++ lib/src/utils/image_utils.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
-import 'dart:typed_data';
 import 'dart:ui' as ui;
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 class ImageUtils {
   /// Base64エンコードされた画像データをデコードしてui.Imageオブジェクトを生成する
@@ -11,10 +13,58 @@ class ImageUtils {
     return frame.image;
   }
 
+  /// Base64エンコード処理をバックグラウンドで実行する
+  static Future<String> _encodeToBase64(Uint8List bytes) {
+    return compute((Uint8List bytes) => base64Encode(bytes), bytes);
+  }
+
   /// ui.Imageオブジェクトをbase64エンコードされた文字列に変換する
-  static Future<String> encodeImageToBase64(ui.Image image) async {
-    final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+  static Future<String> encodeImageToBase64(
+    ui.Image image, {
+    int maxWidth = 1920, // 最大幅[px]
+    int maxHeight = 1080, // 最大高さ[px]
+  }) async {
+    // 1. まず画像をリサイズ（大きすぎる場合）
+    ui.Image processedImage = image;
+
+    if (image.width > maxWidth || image.height > maxHeight) {
+      processedImage = await _resizeImage(image, maxWidth, maxHeight);
+    }
+
+    // 2. 最適化された設定でバイトデータに変換
+    // FIXME: ここで UI スレッドのブロッキングが発生するが、 UI スレッドでしか実行できないので諦める
+    final byteData = await processedImage.toByteData(
+      format: ui.ImageByteFormat.png,
+    );
+
     final bytes = byteData?.buffer.asUint8List() ?? Uint8List(0);
-    return base64Encode(bytes);
+
+    final encodedImage = await _encodeToBase64(bytes);
+
+    return encodedImage;
+  }
+
+  /// サイズを指定してイメージをリサイズする
+  static Future<ui.Image> _resizeImage(ui.Image image, int targetWidth, int targetHeight) async {
+    final pictureRecorder = ui.PictureRecorder();
+    final canvas = Canvas(pictureRecorder);
+
+    // アスペクト比を保持してリサイズ
+    final double scaleW = targetWidth / image.width;
+    final double scaleH = targetHeight / image.height;
+    final double scale = scaleW < scaleH ? scaleW : scaleH;
+
+    final width = (image.width * scale).round();
+    final height = (image.height * scale).round();
+
+    canvas.drawImageRect(
+      image,
+      Rect.fromLTWH(0, 0, image.width.toDouble(), image.height.toDouble()),
+      Rect.fromLTWH(0, 0, width.toDouble(), height.toDouble()),
+      Paint()..filterQuality = FilterQuality.medium,
+    );
+
+    final picture = pictureRecorder.endRecording();
+    return await picture.toImage(width, height);
   }
 }
