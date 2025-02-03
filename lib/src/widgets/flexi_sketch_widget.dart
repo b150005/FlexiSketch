f1e45c79f0edba6009842778d45ea508c15df2cd
b150005 <b150005@outlook.jp>
2025-02-03 11:02:33 +0900

    🔥 不要なimport文の削除

--- lib/src/widgets/flexi_sketch_widget.dart
+++ lib/src/widgets/flexi_sketch_widget.dart
@@ -1,7 +1,6 @@
 import 'dart:developer' as developer;
 import 'dart:ui';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
